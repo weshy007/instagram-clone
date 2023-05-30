6c038a9f77cd490273310456c2f4c5f1e3bcc7c1
Weshy Joe <josephwaweru96@gmail.com>
2023-05-30 13:05:46 +0300

    notofication view

---
+++ notifications/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+import imp
+
+from .models import Notification
+
+
+# Create your views here.
+def show_notification(request):
+    user = request.user
+    notifications = Notification.objects.filter(user=user).order_by('-date')
+
+    context = {
+        'notifications': notifications,
+    }
+
+    return render(request, 'notifications/notification.html', context)
+
+
+def delete_notification(request, id):
+    user = request.user
+    Notification.objects.filter(id=id, user=user).delete()
+    return redirect('show-notification')

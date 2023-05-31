24399365320d9ec580d8abed95b210c4ecd52f48
Weshy Joe <josephwaweru96@gmail.com>
2023-05-31 19:35:54 +0300

    templates styling + url fixes for auth app

--- authentication/urls.py
+++ authentication/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
 
     # Authentication
     path('sign-up/', views.register, name="sign-up"),
-    path('sign-in/', auth_views.LoginView.as_view(template_name="sign-in.html", redirect_authenticated_user=True), name='sign-in'),
-    path('sign-out/', auth_views.LogoutView.as_view(template_name="sign-out.html"), name='sign-out'), 
+    path('sign-in/', auth_views.LoginView.as_view(template_name="sign_in.html", redirect_authenticated_user=True), name='sign-in'),
+    path('sign-out/', auth_views.LogoutView.as_view(template_name="sign_out.html"), name='sign-out'), 
 ]
--- core/settings.py
+++ core/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     
     #Apps 
+    'authentication',
     'posts',
     'notifications',
     'comments',

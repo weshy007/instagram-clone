a6365d185a23bf34dfcbcf5924abcb2d9f6bb5b2
Weshy Joe <josephwaweru96@gmail.com>
2023-06-01 21:06:23 +0300

    p::view-sort imports

--- posts/views.py
+++ posts/views.py
@@ -3,15 +3,13 @@ from django.contrib.auth.models import User
 from django.core.paginator import Paginator
 from django.db.models import Q
 from django.http import HttpResponseRedirect
-from django.shortcuts import get_object_or_404, redirect, render
-# TODO: from authentication.models import Profile
-from django.urls import resolve, reverse
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 
+from authentication.models import Profile
 from comments.forms import NewCommentForm
 from comments.models import Comment
-from authentication.models import Profile
 
-from .forms import NewPostForm
 from .models import Follow, Likes, Post, Stream, Tag
 
 
